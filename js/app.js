c13c36bbff7ce9b0cfd49896a0cd172e646c994e
calculito <ion.oaie@gmail.com>
2020-06-04 13:21:41 +0200

    hotel
    hotel

---
+++ js/app.js
@@ -0,0 +1,54 @@
+const app = new Vue({
+	el:'#app',
+	data: {
+		chat:'',
+		msg:'',
+		eliza:null
+	},
+	created() {
+		this.eliza = new ElizaBot();
+		this.chat = 'Ion: '+this.eliza.getInitial();
+	},
+	methods: {
+		speak() {
+			let reply = this.eliza.transform(this.msg);
+			this.chat += `<br/>You: ${this.msg}<br/>Ion: ${reply}`;
+			this.msg = '';
+			this.$nextTick(() => {
+				// https://stackoverflow.com/a/40737063/52160
+				this.$refs.chatBox.scrollTop = this.$refs.chatBox.scrollHeight;
+
+				if(this.eliza.quit) {
+					alert('Your conversation is now over.');
+					window.location.reload(true);
+				}
+			});
+		}
+	}
+});
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
--- js/diagonale.js
+++
@@ -1,7 +0,0 @@
-
- var was = document.getElementById("dashier");
- function linie(){
-for(i=1; i>=100; i++){
- alert(i);
- was.style.width = i+"%";}
-  }
